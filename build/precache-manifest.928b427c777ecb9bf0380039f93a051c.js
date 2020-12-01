65ee4b980f758e6284abb974627db8c087cba23a
Alex <klealekle@gmail.com>
2020-12-01 23:19:07 +0300

    build

---
+++ build/precache-manifest.928b427c777ecb9bf0380039f93a051c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "847628b4f1efe31b3650ec7610331cfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb73e81c3a45bb090d92",
+    "url": "/static/css/2.7a75a24e.chunk.css"
+  },
+  {
+    "revision": "f7bd5e77f2f66863089f",
+    "url": "/static/css/main.16d6ece0.chunk.css"
+  },
+  {
+    "revision": "eb73e81c3a45bb090d92",
+    "url": "/static/js/2.8743c072.chunk.js"
+  },
+  {
+    "revision": "f86a4e17d3cb58f717d9f416937db9aa",
+    "url": "/static/js/2.8743c072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7bd5e77f2f66863089f",
+    "url": "/static/js/main.efb60e81.chunk.js"
+  },
+  {
+    "revision": "c63617fb22805bf2a9a7",
+    "url": "/static/js/runtime-main.f848c31e.js"
+  },
+  {
+    "revision": "98279f8f8463aabd151af0d4951ca41a",
+    "url": "/static/media/preload.98279f8f.gif"
+  }
+]);
